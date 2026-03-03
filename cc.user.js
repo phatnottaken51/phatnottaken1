de5e156f939bfb4eb1fa8fdd87ce42e130938c2b
phatnottaken51 <vanphat51nbs2010@gmail.com>
2026-03-03 17:41:02 +0700

    Update cc.user.js

--- cc.user.js
+++ cc.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      6.3.2
 // @description  cocaiconcac
-// @source       
+// @source       https://github.com/phatnottaken51/phatnottaken1/raw/refs/heads/main/duma.js
 // @match        *://maxtask.net/*
 // @match        *://*.maxtask.net/*
 // @grant        none
