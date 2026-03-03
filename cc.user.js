556f98e3bab7e0a6b1cc4d37c17c367ab5631ef1
phatnottaken51 <vanphat51nbs2010@gmail.com>
2026-03-03 17:39:15 +0700

    Create cc.user.js

---
+++ cc.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         NguvailozMaxtask
+// @namespace    http://tampermonkey.net/
+// @version      6.3.2
+// @description  cocaiconcac
+// @source       
+// @match        *://maxtask.net/*
+// @match        *://*.maxtask.net/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
